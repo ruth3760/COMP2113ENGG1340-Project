18a1c940e401a8686dbcd5f2ca049fe974a2d9d3
ruth3760 <u3638471@connect.hku.hk>
2025-11-28 14:30:06 +0800

    Update savegame.h

--- savegame.h
+++ savegame.h
@@ -1,14 +1,1 @@
-#ifndef SAVEGAME_H
-#define SAVEGAME_H
 
-#include <string>
-
-class Player;
-
-namespace SaveGame {
-    bool save(const Player &p, int currentWeek, int scenario, const std::string &path = "savegame.txt");
-    bool load(Player &p, int &currentWeek, int &scenario, const std::string &path = "savegame.txt");
-    bool clear(const std::string &path = "savegame.txt");
-}
-
-#endif // SAVEGAME_H
